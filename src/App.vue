b03a1f48c0edeff99b8f70cc4eb0df17823189c0
shohei imaeda <dis.ma.magic1122@gmail.com>
2020-08-17 19:57:34 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="main">
+      <Number :number="score_1p"/>
+      <Number :number="score_2p"/>
+      <button v-on:click="openModal">open</button>
+    </div>
+    <div id="overlay" v-show="showContent" v-on:click.self="closeModal">
+      <div id="content">
+        <p>これがモーダルウィンドウです。</p>
+        <p><button v-on:click="increase">++</button></p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Number from './components/Number.vue'
+
+import 'normalize.css'
 
 export default {
   name: 'App',
   components: {
+    Number,
     HelloWorld
+  },
+  data(){
+    return {
+      score_1p: 1,
+      score_2p: 5,
+      showContent: false
+    }
+  },
+  methods:{
+    openModal: function(){
+      this.showContent = true
+    },
+    closeModal: function(){
+      this.showContent = false
+    },
+    increase: function(){
+      this.score_1p += 1
+    }
   }
 }
 </script>
@@ -23,6 +54,43 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: #D8FFA6;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+}
+
+#main {
+  height: 94vh;
+  background: white;
+  margin: 3vmin;
+  position: relative;
+  border-radius: 4%;
+}
+
+#overlay{
+  /*　要素を重ねた時の順番　*/
+  z-index:1;
+
+  /*　画面全体を覆う設定　*/
+  position:fixed;
+  top:0;
+  left:0;
+  width:100%;
+  height:100%;
+  background-color:rgba(0,0,0,0.5);
+
+  /*　画面の中央に要素を表示させる設定　*/
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+}
+
+#content{
+  z-index:2;
+  width:50%;
+  padding: 1em;
+  background:#fff;
 }
 </style>
---
+++ src/components/Number.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="number">
+    <h1>{{ number }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Number',
+  props: {
+    number: Number
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
